e6180c4df2f71b57a171121fa9effb180c295bc5
arnaud4d <arnaud.schmitt@4d.com>
2021-04-23 14:31:45 +0200

    Firstcommit

---
+++ sidebars.js
@@ -0,0 +1,19 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Docusaurus Tutorial',
+      items: [
+        'getting-started',
+        'create-a-page',
+        'create-a-document',
+        'create-a-blog-post',
+        'markdown-features',
+        'thank-you',
+	]},{ 
+      type: 'link',
+      label: 'Test',
+      href: 'https://doc.4d.com',
+      },
+  ],
+};
