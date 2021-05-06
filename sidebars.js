79c9d745ca3c569e0cb9c40f7bc0cc12a08f7d85
arnaud4d <arnaud.schmitt@4d.com>
2021-05-06 17:27:44 +0200

    fix sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,11 @@ module.exports = {
   mySidebar: [
     {
       type: 'category',
+      label: 'Test',
+      items: ['4D/mdx-example','4D/test-page']
+	},
+	{
+      type: 'category',
       label: 'Getting started',
       items: ['Getting-started/requirements']
 	},
@@ -17,9 +22,3 @@ module.exports = {
 	} ,
   ],
 };
-
-// {
-//   type: "link",
-//   label: "Test",
-//   href: "https://doc.4d.com",
-// },
