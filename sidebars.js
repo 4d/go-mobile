adc0663121344ebf1a930b60e7748887e5337695
Meriem4d <44900125+Meriem4d@users.noreply.github.com>
2021-06-08 14:59:04 +0200

    Upload sidebar Upload sidebar
    	# Please enter the commit message for your changes. Lines starting

--- sidebars.js
+++ sidebars.js
@@ -1,52 +1,87 @@
 module.exports = {
+<<<<<<< HEAD
   docs: [
     {
-      type: "category",
-      label: "Getting started",
+      type: 'category',
+      label: 'Getting started',
       items: [
-        "getting-started/requirements",
-        "getting-started/create-your-first-app",
-      ],
-    },
-    {
-      type: "category",
-      label: "Project definition",
+	            'getting-started/requirements',
+	            'getting-started/create-your-first-app-with-4d-for-ios'
+	   ]
+	},
+	{
+      type: 'category',
+      label: 'Project definition',
       items: [
-        "project-definition/overview",
-        "project-definition/general",
-        "project-definition/structure",
-      ],
-    },
-    {
-      type: "category",
-      label: "Extra features",
+	            'project-definition/overview',
+				'project-definition/general', 
+				'project-definition/structure', 
+				'project-definition/structure-relations-many-to-many-relations', 
+				'project-definition/structure-one-to-many-relations-actions',
+				'project-definition/structure-one-to-many-relations-custom-button',
+				'project-definition/structure-relations-one-to-many-relations-title-definition',
+				'project-definition/structure-relation-interactions', 
+				'project-definition/structure-relations-many-to-one-relations', 
+				'project-definition/data', 
+				'project-definition/data-restricted-queries-introduction', 
+				'project-definition/data-restricted-queries-define-filter-query', 
+				'project-definition/data-restricted-queries-user-information-query', 
+				'project-definition/actions', 
+				'project-definition/actions-getting-started',
+				'project-definition/actions-define-first-action', 
+				'project-definition/actions-include-action-custom-template', 
+				'project-definition/actions-parameters', 
+				'project-definition/labels-and-icons', 
+				'project-definition/labels-and-icons-data-formatter',
+				'project-definition/labels-and-icons-create-data-formatter', 
+				'project-definition/labels-and-icons-data-formatter-build-and-run-project',
+				'project-definition/labels-and-icons-icons-getting-started', 
+				'project-definition/labels-and-icons-create-icons',
+				'project-definition/labels-and-icons-include-icons',
+				'project-definition/labels-and-icons-select-icons',
+				'project-definition/labels-and-icons-mobile-project',
+				'project-definition/main-menu', 'project-definition/forms',
+				'project-definition/forms-multi-critera-search',
+				'project-editor/publishing'
+				
+		],     
+	},
+	{
+      type: 'category',
+      label: 'Extra features',
       items: [
-        "extra-features/deep-linking",
-        "extra-features/email-authentication",
-        "extra-features/push-notification",
-      ],
-    },
-    {
-      type: "category",
-      label: "Testing projects",
+	             'extra-features/deep-linking',
+				 'extra-features/email-authentication', 
+				 'extra-features/push-notification'
+			],     
+	},
+	{
+      type: 'category',
+      label: 'Testing projects',
       items: [
-        "testing-projects/testing-on-your-device",
-        "testing-projects/using-the-simulator",
-      ],
-    },
-    {
-      type: "category",
-      label: "Customizing projects",
+	              'testing-projects/testing-on-your-device',
+	              'testing-projects/using-the-simulator'],     
+	},
+	{
+      type: 'category',
+      label: 'Customizing projects',
       items: [
-        "customizing-projects/listform-create-template",
-        "customizing-projects/gallery-update-template",
-      ],
-    },
-    {
-      type: "category",
-      label: "Deployment",
-      items: ["deployment/deploy-on-app-store-archive-and-upload"],
-    },
+	               'customizing-projects/listform-create-template',
+				   'customizing-projects/gallery-update-template'],     
+	},
+	{
+      type: 'category',
+      label: 'Deployment',
+      items: [
+	                'deployment/deploy-on-app-store-archive-and-upload',
+					'deployment/deployment-in-house-archive-and-export', 
+					'deployment/deployment-in-house-distribute',
+					'deployment/deploy-on-app-store-create-appstore-record', 
+					'deployment/selecting-your-developer program', 
+					'deployment/selecting-your-developer-program-register-apple-developer-enterprise-program', 
+					'deployment/selecting-your-developer-program-register-apple-developer-program-individual', 
+					'deployment/selecting-your-developer-program-register-apple-developer-program-organization'],     
+	},
     {
       type: "category",
       label: "Test",
@@ -176,5 +211,6 @@ module.exports = {
         "tutorials/features/deep-linking",
       ],
     },
+
   ],
 };
