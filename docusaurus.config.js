28ed32411b50713f199214f86496edd824b315bb
arnaud4d <arnaud.schmitt@4d.com>
2021-07-05 16:53:40 +0200

    fixed edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -173,7 +173,7 @@ module.exports = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+			"https://github.com/4d/go-mobile/edit/main",
         },
         blog: {
           showReadingTime: true,
